e5b6fab297e3df38e3c42550165bd84aecc00154
willard379 <willard379@gmail.com>
2018-01-01 12:14:12 +0900

    トップ画像を表示するようにした

--- src/main/java/hoshisugi/rukoru/app/view/ContentController.java
+++ src/main/java/hoshisugi/rukoru/app/view/ContentController.java
@@ -5,6 +5,7 @@ import java.io.UncheckedIOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import hoshisugi.rukoru.app.view.content.EC2ContentController;
@@ -13,10 +14,19 @@ import hoshisugi.rukoru.app.view.content.S3ContentController;
 import hoshisugi.rukoru.flamework.controls.BaseController;
 import hoshisugi.rukoru.flamework.util.ConcurrentUtil;
 import hoshisugi.rukoru.flamework.util.FXUtil;
+import javafx.application.Platform;
+import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
 
 public class ContentController extends BaseController {
 
@@ -27,6 +37,7 @@ public class ContentController extends BaseController {
 
 	@Override
 	public void initialize(final URL url, final ResourceBundle resource) {
+		showTopImage();
 		ConcurrentUtil.run(() -> {
 			loadContent(EC2ContentController.class);
 			loadContent(RepositoryDBContentController.class);
@@ -52,4 +63,23 @@ public class ContentController extends BaseController {
 			throw new UncheckedIOException(e);
 		}
 	}
+
+	private void showTopImage() {
+		ConcurrentUtil.run(() -> {
+			final ImageView topImage = new ImageView(new Image(
+					"https://s3-ap-northeast-1.amazonaws.com/com.appresso.dsc.redmine/assets/rukoru/top.jpg"));
+			topImage.setPreserveRatio(true);
+			final BorderPane parent = (BorderPane) layoutRoot.getParent();
+			final Optional<ReadOnlyDoubleProperty> left = Optional.ofNullable(parent.getLeft()).map(Region.class::cast)
+					.map(Region::widthProperty);
+			final Optional<ReadOnlyDoubleProperty> right = Optional.ofNullable(parent.getRight())
+					.map(Region.class::cast).map(Region::widthProperty);
+			final DoubleProperty defaultValue = new SimpleDoubleProperty(0d);
+			final DoubleBinding fitWidth = parent.widthProperty().subtract(10d).subtract(left.orElse(defaultValue))
+					.subtract(right.orElse(defaultValue));
+			topImage.fitWidthProperty().bind(fitWidth);
+			Platform.runLater(() -> layoutRoot.getChildren().add(topImage));
+		});
+	}
+
 }
